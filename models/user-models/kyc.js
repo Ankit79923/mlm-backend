c6ab6c272e479e589055e2fcc90909b56a611061
Ankit <ankit.synthospheretech@gmail.com>
2024-10-23 14:34:06 +0530

    Done

---
+++ models/user-models/kyc.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+
+const kycSchema = mongoose.Schema({
+    userDetails: {
+        mySponsorId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        mobileNumber: {
+            type: Number,
+            required: true
+        }
+    },
+    bankDetaills: {
+        bankName : {
+            type: String,
+            required: true
+        },
+        branchName: {
+            type: String,
+            required: true
+        },
+        accountNumber : {
+            type: Number,
+            required: true
+        },
+        ifscCode : {
+            type: String,
+            required: true
+        },
+        panCard : {
+            type: String,
+            required: true
+        },
+        aadharCard : {
+            type: Number,
+            required: true
+        }
+    },
+    documents: {
+        panCardFront: {
+            type: String, // Store the path or URL of the uploaded image
+            required: true
+        },
+        aadharCardFront: {
+            type: String,
+            required: true
+        },
+        aadharCardBack: {
+            type: String, 
+            required: true
+        },
+        bankCard: {
+            type: String, 
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+});
+
+
+
+
+const KYC = mongoose.model('KYC', kycSchema);
+module.exports = KYC;
