dcd71453e8155ab4299969b2ccbef9d2b8d1d318
Ankit79923 <ankit.synthospheretech@gmail.com>
2024-12-19 22:14:24 +0530

    Update calculatePayout

--- utils/calculatePayout.js
+++ utils/calculatePayout.js
@@ -18,7 +18,8 @@ const calculateWeekelyPayout = async (req, res) => {
 
       // Calculate matched BV & payout
       const matchedBV = Math.min(leftBV, rightBV);
-      const payoutAmount = matchedBV * 0.1;
+      const directBVamount = matchedBV * 0.1;
+      const payoutAmount = directBVamount + totalBV;
 
       // Create a new weekly earning entry
       const newEarning = {
@@ -58,7 +59,8 @@ const calculateMonthlyPayout = async function (req, res) {
 
         // Calculate Monthly payoutAmount
         const matchedBV = Math.min(leftBV, rightBV);
-        const payoutAmount = matchedBV * 0.1;
+        const directBVamount = matchedBV * 0.1;
+        const payoutAmount = directBVamount + totalBV;
 
         // Create & save new monthly earning entry
         const newMonthlyEarning = { month: todayDate, payoutAmount };
