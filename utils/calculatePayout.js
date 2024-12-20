362f3dcf9dc15efd7521042aef7f583901cafeb2
srijanibanerje <srijaniofficial2022@gmail.com>
2024-12-20 14:06:46 +0530

    Update calculatePayout

--- utils/calculatePayout.js
+++ utils/calculatePayout.js
@@ -18,8 +18,7 @@ const calculateWeekelyPayout = async (req, res) => {
 
       // Calculate matched BV & payout
       const matchedBV = Math.min(leftBV, rightBV);
-      const totalBV = leftBV + rightBV; // Calculate total BV
-      const payoutAmount = matchedBV * 0.1 + totalBV * 0.1;
+      const payoutAmount = matchedBV * 0.1;
 
       // Create a new weekly earning entry
       const newEarning = {
