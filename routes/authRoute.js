b4050f4018759688dc9c0cf3b07458a465e329bd
srijanibanerje <srijaniofficial2022@gmail.com>
2024-12-23 18:03:45 +0530

    "done"

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-const {handleRegisterUser, handleLoginUser, handleRegisterUsingLeftLink, handleRegisterUsingRightLink, handleFindUser, handleRegisterFirstUser, handleVerifySponsor  } = require('../controllers/authController');
+const {handleRegisterUser, handleLoginUser, handleRegisterUsingLeftLink, handleRegisterUsingRightLink, handleFindUser, handleRegisterFirstUser, handleVerifySponsor, handleAllUser, handleUserbyitsid  } = require('../controllers/authController');
 const { searchproduct } = require('../controllers/productController');
 
 router.post('/registerFirstUser', handleRegisterFirstUser);
@@ -12,7 +12,10 @@ router.post('/registerRight', handleRegisterUsingRightLink);
 router.post('/login', handleLoginUser);
 router.get('/findUser/:id', handleFindUser);            // R
 router.post('/verifySponsor', handleVerifySponsor);     // R
-router.get('/searchproduct', searchproduct)
+router.get('/searchproduct', searchproduct);
+router.get('/searchproduct', searchproduct);
+router.get('/handleAllUser', handleAllUser);
+router.get('/handleUserbyitsid/:id', handleUserbyitsid);
 
 
 module.exports = router;
